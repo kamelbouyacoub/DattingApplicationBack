888790caa945958e82ae1e099c1976ad3fd9b230
BOUYACOUB Kamel <kamel_bouyacoub07@live.fr>
2021-03-25 22:23:36 +0100

    - Add ssl cors

--- DattingApplication/Startup.cs
+++ DattingApplication/Startup.cs
@@ -49,7 +49,7 @@ namespace DattingApplication
             //app.UseHttpsRedirection();
 
             app.UseRouting();
-            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
